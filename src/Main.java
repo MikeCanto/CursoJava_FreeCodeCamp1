41878955dbc070bb91b84684e827eecdfcc3f15e
MikeCanto <mj_cantom@protonmail.com>
2023-01-25 10:18:59 -0600

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("my name is mike si soy Miguel world!");
+        int age;
+        age  = 10;
     }
 }
